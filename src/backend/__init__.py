673c9a0b80e7d23c5d3eb2eddf3d25a7795766c9
Hussain Parsaiyan <hussain.parsaiyan@iceanimations.com>
2014-03-08 17:18:48 +0500

    checkout function changed to take in a snapshot search key. File opening functionality added too.

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -2,7 +2,7 @@ from site import addsitedir as asd
 from . import _backend
 reload(_backend)
 from ._backend import *
+
 asd(r"r:/Pipe_Repo/Users/Hussain/utilities")
 asd(r"r:/Pipe_Repo/Projects/TACTIC")
-
 
--- src/backend/_backend.py
+++ src/backend/_backend.py
@@ -1,12 +1,19 @@
 from auth import user
 
-def checkout(sobj, context):
+import pymel.core as pc
+
+def checkout(snapshot):
     '''
-    @snapshot: search_key  and context of sobject whose snapshot is to be checked out
+    @snapshot: snapshot search_key
     '''
+    server = user.get_server()
     if user.user_registered():
         server = user.get_server()
-        paths = server.checkout(sobj, context, to_sandbox_dir = True)
+        snap = server.get_by_search_key(snapshot)
+        sobj = server.get_by_search_key(server.build_search_key(snap['search_type'], snap['search_code'], snap['project_code']))
+
+        paths = server.checkout(sobj['__search_key__'], snap['context'], to_sandbox_dir = True)
+        pc.openFile(paths[0], force = True)
         return paths[0]
         
         
