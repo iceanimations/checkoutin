45138ded5ead0397f8304f8fe82a66c5e5a0bdd7
Hussain Parsaiyan <hussain.parsaiyan@iceanimations.com>
2014-03-10 15:33:35 +0500

    some bug fixed

--- src/backend/_backend.py
+++ src/backend/_backend.py
@@ -29,6 +29,7 @@ def checkout(snapshot):
         server = user.get_server()
         snap = server.get_by_search_key(snapshot)
         sobj = server.get_by_search_key(server.build_search_key(snap['search_type'], snap['search_code'], snap['project_code']))
+        file_type = server.get_by_search_key(server.query('sthpw/file', filters = [('snapshot_code', snap['code']), ('project_code', snap['project_code'])])[0]['__search_key__'])
         paths = server.checkout(sobj['__search_key__'], snap['context'], to_sandbox_dir = True)
         pc.openFile(paths[0], force = True)
         return paths[0]
