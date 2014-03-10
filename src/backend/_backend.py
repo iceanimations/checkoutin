24bddbc445aa71248375b62365d777d20efe624d
Hussain Parsaiyan <hussain.parsaiyan@iceanimations.com>
2014-03-10 14:22:41 +0500

    checkout functionality added

--- src/backend/_backend.py
+++ src/backend/_backend.py
@@ -1,6 +1,24 @@
 from auth import user
-
+import tempfile
 import pymel.core as pc
+import tactic_client_lib.application.maya as maya
+import datetime
+import os
+import os.path as op
+dt = datetime.datetime
+m = maya.Maya()
+def getTemp(mkd = False, suffix = "", prefix = "tmp", directory = None):
+    tmp = getattr(tempfile,
+                  "mkdtemp" if mkd else "mkstemp")(suffix = suffix,
+                                                   prefix = prefix,
+                                                   dir = directory)
+    if mkd: return tmp
+    else:
+        os.close(tmp[0])
+        return tmp[1]
+
+
+
 
 def checkout(snapshot):
     '''
@@ -11,9 +29,26 @@ def checkout(snapshot):
         server = user.get_server()
         snap = server.get_by_search_key(snapshot)
         sobj = server.get_by_search_key(server.build_search_key(snap['search_type'], snap['search_code'], snap['project_code']))
-
         paths = server.checkout(sobj['__search_key__'], snap['context'], to_sandbox_dir = True)
         pc.openFile(paths[0], force = True)
         return paths[0]
         
         
+
+
+def checkin(sobject, context, version=-1):
+    '''
+    @sobject: search_key of sobject to which the checkin belongs
+    @context: context of the sobject
+    @version: version number of the snapshot (functionality not implemented)
+    '''
+    
+    server = user.get_server()
+    tmpfile = op.normpath(getTemp(prefix = dt.now().strftime("%Y-%M-%d %H-%M-%S"))).replace("\\", "/")
+    print tmpfile
+    save_path = m.save(tmpfile, file_type = "mayaBinary" if pc.sceneName().endswith(".mb") else "mayaAscii")
+    snapshot = user.get_server().simple_checkin(sobject, context, save_path, use_handoff_dir=True, mode = 'copy')
+    search_key = snapshot['__search_key__']
+    path = checkout(search_key)
+    return {search_key: op.basename(path)}
+    
