9946987c5a9a94e3cd5b83f89ef5d19fd9c48243
Hussain Parsaiyan <hussain.parsaiyan@iceanimations.com>
2014-04-02 11:51:46 +0500

    referencing feature implemented
    Maya files will now self-contain the information of which asset's snapshot it has referenced

--- src/backend/_backend.py
+++ src/backend/_backend.py
@@ -2,10 +2,12 @@ from auth import user
 import util
 import tempfile
 import pymel.core as pc
+import imaya as mi
 import tactic_client_lib.application.maya as maya
 import datetime
 import os
 import os.path as op
+import json
 
 dt = datetime.datetime
 m = maya.Maya()
@@ -20,10 +22,7 @@ def getTemp(mkd = False, suffix = "", prefix = "tmp", directory = None):
         os.close(tmp[0])
         return tmp[1]
 
-
-
-
-def checkout(snapshot):
+def checkout(snapshot, r = False):
     '''
     @snapshot: snapshot search_key
     '''
@@ -43,16 +42,94 @@ def checkout(snapshot):
                                     ('project_code',
                                      snap['project_code'])])[0]
             ['__search_key__'])
-        paths = server.checkout(sobj['__search_key__'],
-                                snap['context'],
-                                to_sandbox_dir = True,
-                                version = snap['version'],
-                                file_type = file_type["type"])
-        pc.openFile(paths[0], force = True)
-        return paths[0]
-        
-        
+        if not r:
+            paths = server.checkout(sobj['__search_key__'],
+                                    snap['context'],
+                                    to_sandbox_dir = True,
+                                    version = snap['version'],
+                                    file_type = file_type["type"])
+            pc.openFile(paths[0], force = True)
+            return paths[0]
 
+        else:
+            return _reference(snap)
+
+def _reference(snapshot):
+
+    server = user.get_server()
+    filename = util.filename_from_snap(snapshot, mode = 'client_repo')
+    try:
+        mi.addReference(paths = [filename])
+    except:
+        pass
+    
+    tactic_raw = mi.FileInfo.get('TACTIC')
+    
+    if tactic_raw:
+        tactic = json.loads(tactic_raw)
+    
+    else:
+        tactic = {}
+
+    tactic['__ver__'] = "0.1"
+    
+    assets = tactic.get('assets', [])
+    # just to ensure the 'assets' key is part of the dictionary
+    tactic['assets'] = assets
+    present = False
+    for asset in assets:
+        if (asset.get('search_type') == snapshot.get('search_type') and
+            asset.get('search_code') == snapshot.get('search_code') and
+            asset.get('project_code') == snapshot.get('project_code') and
+            asset.get('process') == snapshot.get('process') and
+            asset.get('context') == snapshot.get('context')):
+            present = True
+            return True
+            
+    # {"is_synced": true, 
+    #  "code": "SNAPSHOT", 
+    #  "process": "model", 
+    #  "s_status": null, 
+    #  "id": 1123, 
+    #  "column_name": "snapshot", 
+    #  "label": null, 
+    #  "project_code": "_3", 
+    #  "is_latest": true, 
+    #  "level_id": null, 
+    #  "lock_login": null, 
+    #  "lock_date": null, 
+    #  "version": 1, 
+    #  "__search_key__": "sthpw/snapshot?code=SNAPSHOT", 
+    #  "level_type": null, 
+    #  "search_id": 13, 
+    #  "metadata": null, 
+    #  "status": null, 
+    #  "description": "No description", 
+    #  "timestamp": "2041-30-29 21:50:14.459963", 
+    #  "repo": null, 
+    #  "is_current": true, 
+    #  "search_code": "r_dd", 
+    #  "snapshot_type": "file", 
+    #  "server": null, 
+    #  "search_type": "vfx/asset?project=_3", 
+    #  "snapshot": "<snapshot timestamp=\"asdfs\" context=\"model\" search_key=\"vfx/asset?project=_3&amp;code=_d\" login=\"foo.bar\" checkin_type=\"strict\">\n  <file file_code=\"FILE\" name=\"_d_model_v001.ma\" type=\"main\"/>\n</snapshot>\n", 
+    #  "context": "model", 
+    #  "login": "foo.bar", 
+    #  "revision": 0
+    # }
+    map(snapshot.pop, ['is_synced', 'code', 's_status', 'id',
+                       'column_name', 'label','is_latest',
+                       'level_id', 'lock_login', 'lock_date',
+                       'version', '__search_key__', 'level_type',
+                       'search_id', 'metadata', 'status',
+                       'description', 'timestamp', 'repo',
+                       'is_current', 'snapshot_type', 'server',
+                       'snapshot', 'login', 'revision'])
+            
+    assets.append(snapshot)
+    mi.FileInfo.save('TACTIC', json.dumps(tactic))
+    return True
+        
 
 def checkin(sobject, context, process = None,
             version=-1, description = 'No description',
@@ -70,8 +147,10 @@ def checkin(sobject, context, process = None,
                                   strftime("%Y-%M-%d %H-%M-%S")
                               )).replace("\\", "/")
 
-    save_path = m.save(tmpfile, file_type = "mayaBinary"
-                       if pc.sceneName().endswith(".mb") else "mayaAscii") if not file else file
+    save_path = (m.save(tmpfile, file_type = "mayaBinary"
+                        if pc.sceneName().endswith(".mb")
+                        else "mayaAscii")
+                 if not file else file)
     
     if process != context:
         context = '/'.join([process, context])
