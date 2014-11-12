ae03cf0570deb10575e2f384804837c7af55584d
Qurban Ali <qurban_ali36@yahoo.com>
2014-11-12 12:28:05 +0500

    assetsExplorer renamed to mainBrowser

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 import src.myTasks as mt
-import src.assetsExplorer as ae
+import src.mainBrowser as ae
 import src._seqshot as ss
 reload(ss)
 reload(ae)
 reload(mt)
 MyTasks = mt.MyTasks
-AssetsExplorer = ae.AssetsExplorer
+MainBrowser = ae.MainBrowser
 ShotExplorer = ss.ShotExplorer
