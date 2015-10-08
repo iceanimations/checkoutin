142ab4caecbf60361c5c1c9097d42a2f49f7f577
Talha Ahmed <talha.ahmed@gmail.com>
2015-10-08 19:20:34 +0500

    adding publish report to checkoutin init

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
 import src.myTasks as mt
 import src.mainBrowser as ae
 import src._seqshot as ss
+import src.published_report as pr
 reload(ss)
 reload(ae)
 reload(mt)
+reload(pr)
 MyTasks = mt.MyTasks
 MainBrowser = ae.MainBrowser
 ShotExplorer = ss.ShotExplorer
+PublishReport = pr.PublishReport
